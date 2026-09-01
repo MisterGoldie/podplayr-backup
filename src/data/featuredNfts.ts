e683e07bd8421ec311026824acb3a62049549f27
MisterGoldie <scarygarysnft@gmail.com>
2026-09-01 06:29:34 -0500

    changed sazons video thumbnail start time image

--- src/data/featuredNfts.ts
+++ src/data/featuredNfts.ts
@@ -163,7 +163,7 @@ export const FEATURED_NFTS: NFT[] = [
   {
     name: 'I Asked My Friends A Serious Question',
     image:
-      'https://image.mux.com/1C023gIJ9baWRdDavLYzKqB02iBPUHeO00wfTaL2AnGp00s/thumbnail.jpg?time=10',
+      'https://image.mux.com/1C023gIJ9baWRdDavLYzKqB02iBPUHeO00wfTaL2AnGp00s/thumbnail.jpg?time=11',
     contract: 'pending',
     tokenId: 'iasked-friends-serious-question',
     network: 'base',
