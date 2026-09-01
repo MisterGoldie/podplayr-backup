0b1f508042b4c7a7b3ab219925b2f9f147fe77bb
MisterGoldie <scarygarysnft@gmail.com>
2026-08-31 20:35:52 -0500

    removed live from explore page

--- src/components/views/ExploreView.tsx
+++ src/components/views/ExploreView.tsx
@@ -24,7 +24,6 @@ import { getMediaKey } from '../../utils/media';
 import type { NFT } from '../../types/nft';
 import ProfileAvatar from '../user/ProfileAvatar';
 import { CommunityPills } from '../user/CommunityPills';
-import { LiveStreamFrame } from '../live/LiveStreamFrame';
 
 type ExploreFilter = 'all' | 'farcaster' | 'ens';
 
@@ -518,12 +517,6 @@ const ExploreView: React.FC<ExploreViewProps> = (props) => {
             </div>
             {hasMoreSearch && <div ref={searchSentinelRef} className="h-8" aria-hidden="true" />}
           </section>
-        )}
-
-        {showDiscovery && filter !== 'ens' && (
-          <div className="px-4">
-            <LiveStreamFrame />
-          </div>
         )}
 
         {showDiscovery && featuredReady && (
